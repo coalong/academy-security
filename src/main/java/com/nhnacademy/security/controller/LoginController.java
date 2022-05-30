83a6926279dcd212f6041a1e1a7c8b8a7866c43c
coalong <minay0904@gmail.com>
2022-05-30 13:26:01 +0900

    + 로그인 구현중

--- src/main/java/com/nhnacademy/security/controller/LoginController.java
+++ src/main/java/com/nhnacademy/security/controller/LoginController.java
@@ -2,17 +2,21 @@ package com.nhnacademy.security.controller;
 
 import com.nhnacademy.security.service.LoginService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Objects;
 
 @Controller
 public class LoginController {
-
     private final LoginService loginService;
-
     public LoginController(LoginService loginService) {
-        this.loginService = loginService;
-    }
+        this.loginService = loginService;}
 
 
     @GetMapping("/login")
@@ -24,3 +28,32 @@ public class LoginController {
     public
 
 }
+
+//    @GetMapping(value = {"/", "/login"})
+//    public String login(@ModelAttribute("user")  String userId) {
+//        if (Objects.isNull(userId)) {
+//            // 로그인 안되있으면 loginForm
+//            return "index/loginForm";
+//        } else {
+//            return "index/home";
+//        }
+//    }
+//
+//
+//    @PostMapping("/login")
+//    public String doLogin(@RequestParam("id") String id,
+//                          @RequestParam("pwd") String pwd,
+//                          HttpServletRequest request,
+//                          ModelMap modelMap) {
+//        if (userService.matches(id, pwd)) {
+//            //성공시
+//            HttpSession session = request.getSession();
+//            session.setAttribute("id", id);
+//
+//            modelMap.put("id", session.getAttribute("id"));
+//            return "index/loginSuccess";
+//        } else {
+//            //실패시
+//            return "redirect:/login";
+//        }
+//    }
--- src/main/java/com/nhnacademy/security/service/DefaultLoginService.java
+++ src/main/java/com/nhnacademy/security/service/DefaultLoginService.java
@@ -4,22 +4,24 @@ import com.nhnacademy.security.entity.Member;
 import com.nhnacademy.security.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
-public class DefaultLoginService implements LoginService{
+public class DefaultLoginService implements LoginService {
     private final MemberRepository memberRepository;
-
 
     public DefaultLoginService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
-
     @Override
     public boolean matches(String id, String pwd) {
         Optional<Member> member = memberRepository.findById(id);
-
-        return false;
+        if (Objects.equals(member.get().getPwd(), pwd) && Objects.nonNull(member)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
